b911ca85bf8a83578aeaf2e8499bcf31e1723701
«Roman <research777@mail.ru>
2024-01-26 21:04:44 +0300

    Client

--- chat-client/src/main/java/org/example/chat/client/Program.java
+++ chat-client/src/main/java/org/example/chat/client/Program.java
@@ -1,7 +1,40 @@
 package org.example.chat.client;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Client ...");
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter your name: ");
+
+            String name = scanner.nextLine();
+
+            InetAddress address = InetAddress.getLocalHost();
+
+            Socket socket = new Socket(address, 5000);
+            Client client = new Client(socket, name);
+            InetAddress inetAddress = socket.getInetAddress();
+            System.out.println("InetAddress: " + inetAddress);
+            String remoteIp = inetAddress.getHostAddress();
+            System.out.println("Remote IP: " + remoteIp);
+            System.out.println("LocalPort:" + socket.getLocalPort());
+
+            client.listenForMessage();  // Слушатель для входящих сообщений
+            client.sendMessage();       // Отправить сообщение
+
+        }
+        catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
